99a5272528c00cb0f18102c46acef5d67e1a0397
Anders Båtsman <anders@loghouse.dev>
2022-01-27 17:31:49 +0100

    Handle when Discord alert does not responds

--- python/alerts.py
+++ python/alerts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # coding: utf8
+import logging
 
 import requests
 import json
@@ -10,11 +11,15 @@ class alert_module:
         configFile = '/config/config.json'
         with open(configFile,'r') as of:
             self.data = json.load(of)
-    
+
     def discord(self, alert_msg=""):
         alert_info = self.data['Alerts']
         for alert_data in alert_info:
             if alert_data['Alerts-Enabled'] == True:
                 webhook = Webhook.from_url(alert_data['Discord-Webhook'], adapter=RequestsWebhookAdapter())
-                webhook.send(alert_msg)
+
+                try:
+                    webhook.send(alert_msg)
+                except Exception as err:
+                    logging.critical(f'Discord message not sent issue: {err}')
 
